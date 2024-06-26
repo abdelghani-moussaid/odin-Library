24725bc80cf8360a8d752fedcc05c122aab6d991
Abdelghani Moussaid <20841568+abdelghani-moussaid@users.noreply.github.com>
2024-06-26 15:36:35 +0100

    change book status as read or unread

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function Book(title, author, pages, readingStatus) {
     };
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295);
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "Not read yet");
 myLibrary.push(theHobbit);
 
 
@@ -55,7 +55,7 @@ function addBookToLibrary() {
         const myBook = new Book(document.getElementById("title").value, 
                                 document.getElementById("author").value, 
                                 document.getElementById("pages").value,
-                                "no");
+                                document.getElementById("readingStatus").value);
         myLibrary.push(myBook);
         frm.reset(); 
         modal.style.display = "none";
@@ -66,6 +66,7 @@ function addBookToLibrary() {
 }
 addBookToLibrary();
 handleDeletingBooks();
+handleReadingBooks();
 
 function handleDeletingBooks(){
     const deleting = document.querySelectorAll(".deleteBtn");
@@ -82,7 +83,19 @@ function handleDeletingBooks(){
 
 }
 
-
+function handleReadingBooks(){
+    const read = document.querySelectorAll(".readBtn");
+    read.forEach((readBook) => {
+        readBook.addEventListener("click", ()=> {
+            empty(tbody);
+            if(readBook.className === "readBtn")
+                myLibrary[readBook.id].readingStatus = "Read";
+            else
+                myLibrary[readBook.id].readingStatus = "Not read yet";
+            displayBookOnPage();
+        });
+    });
+}
 
 
 function displayBookOnPage() {
@@ -97,20 +110,45 @@ function displayBookOnPage() {
         pages.textContent = book.pages;
         const readingStatus = document.createElement("td");
         readingStatus.textContent = book.readingStatus;
-        const deleting = document.createElement("td");
-        const btn = document.createElement("button");
-        btn.textContent = "delete";
-        btn.id = index;
-        btn.className = "deleteBtn";
-        deleting.appendChild(btn);
+        const action = document.createElement("td");
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.id = index;
+        deleteBtn.className = "deleteBtn";
+        const deleteImg = document.createElement("img");
+        deleteImg.setAttribute("src", "images/delete.svg");
+        deleteImg.setAttribute("title", "delete");
+        deleteImg.id = "deleteImg";
+        deleteBtn.appendChild(deleteImg);
+
+        const readBtn = document.createElement("button");
+        readBtn.id = index;
+        const readImg = document.createElement("img");
+        if(book.readingStatus !== "Read"){
+            readImg.setAttribute("src", "images/read.svg");
+            readImg.setAttribute("title", "mark as read");
+            readBtn.className = "readBtn";
+        } else {
+            readImg.setAttribute("src", "images/notebook-remove.svg");
+            readImg.setAttribute("title", "unread");
+            readBtn.className = "readBtn unreadBtn";
+        }
+
+        readImg.id = "readImg";
+        readBtn.appendChild(readImg);
+
+        action.appendChild(readBtn);
+        action.appendChild(deleteBtn);
+
         tbody.appendChild(row);
         row.appendChild(title);
         row.appendChild(author);
         row.appendChild(pages);
         row.appendChild(readingStatus);
-        row.appendChild(deleting);
+        row.appendChild(action);
     });
-    handleDeletingBooks()
+    handleDeletingBooks();
+    handleReadingBooks();
 }
 
 function empty(element) {
